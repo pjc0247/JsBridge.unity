f7e79ad067bac6f8b1af5dd4e7bbd67c69e76b23
Park June Chul <pjc0247@naver.com>
2019-05-02 19:47:14 +0900

    Create JsBridge.cs

---
+++ src/JsBridge.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+using Newtonsoft.Json;
+
+public class JsBridge : MonoBehaviour
+{
+    public class Packet
+    {
+        public int pid = 0;
+    }
+    public class Js2UnityPacket : Packet
+    {
+        public string function;
+        public string message;
+    }
+    public class Unity2JsPacket : Packet
+    {
+        public string message;
+    }
+
+    [DllImport("__Internal")]
+    private static extern void JsReturn(int pid, string msg);
+
+    private static Dictionary<string, Func<int, string, object>> functions 
+        = new Dictionary<string, Func<int, string, object>>();
+
+    private static int pid = 0;
+    private static GameObject gobj;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        gobj = new GameObject("JsBridge");
+        GameObject.DontDestroyOnLoad(gobj);
+    }
+
+    public static void Add<TIn, TOut>(string name, Func<int, TIn, TOut> callback)
+    {
+        functions[name] = (int pid, string _in) =>
+        {
+            return callback(pid, JsonConvert.DeserializeObject<TIn>(_in));
+        };
+    }
+    public static void Retrun(int pid, object obj)
+    {
+        JsReturn(pid, JsonConvert.SerializeObject(obj));
+    }
+
+    public void OnMessage(string msg)
+    {
+        var packet = JsonConvert.DeserializeObject<Js2UnityPacket>(msg);
+
+        Func<int, string, object> func = null;
+        if (functions.TryGetValue(packet.function, out func))
+        {
+            func(packet.pid, packet.message);
+        }
+        else
+            Debug.LogError("[JsBridge] Unknown funcall: " + packet.function);
+    }
+}
